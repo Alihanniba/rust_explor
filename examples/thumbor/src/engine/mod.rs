912693f761b419eea715cbeaad25eea2af15dcbe
刘立 <liuli.up@bytedance.com>
2023-12-09 18:53:36 +0800

    impl
    Change-Id: I8bb6a43fffd64fe0839b85a708d0bf7dcd29d9dd

---
+++ examples/thumbor/src/engine/mod.rs
@@ -0,0 +1,23 @@
+use crate::pb::Spec;
+use image::ImageOutputFormat;
+
+mod photon;
+pub use photon::Photon;
+
+// Engine trait：未来可以添加更多的engine，主流程只需要替换engine
+pub trait Engine {
+  // 对engine按照specs进行一系列有序的处理
+  fn apply(&mut self, specs: &[Spec]);
+
+  // 从engine中生成目标图片，注意这里是用的self，而非self的引用
+  fn generate(self, format: ImageOutputFormat) -> Vec<u8>;
+}
+
+// 泛型接口
+// SpecTransform 未来如果添加更多的spec，只需要实现它即可
+pub trait SpecTransform<T> {
+  // 对图片使用op做transform
+  fn transform(&mut self, op: T);
+}
+
+
---
+++ examples/thumbor/src/engine/photon.rs
@@ -0,0 +1,48 @@
+use super::{Engine, SpecTransform};
+use crate::pb::*;
+use anyhow::Result;
+use bytes::Bytes;
+use image::{DynamicImage, ImageBuffer, ImageOutputFormat};
+use lazy_static::lazy_static;
+use photon_rs::{
+  effects,
+  filters,
+  multiple,
+  native::open_image_from_bytes,
+  transform,
+  PhotonImage,
+};
+use std::convert::TryFrom;
+
+lazy_static! {
+  static ref WATERMARK: PhotonImage {
+    let data = include_bytes!("../../rust-logo.png");
+    let watermark = open_image_from_bytes(data).unwrap();
+    transform::resize(&watermark, 64,64,transform::SamplingFilter::Nearest)
+  }
+}
+
+pub struct Photon(PhotonImage);
+
+impl TryFrom<Bytes> for Photon {
+  type Error = anyhow::Error;
+
+  fn try_from(data: Bytes) -> Result<Self, Self::Error> {
+    Ok(Self(open_image_from_bytes(&data)?))
+  }
+}
+
+impl Engine for Photon {
+  fn apply(&mut self, specs: &[Spec]) {
+    for spec in specs {
+      match spec {
+        
+      }
+    }
+  }
+}
+
+
+
+
+
